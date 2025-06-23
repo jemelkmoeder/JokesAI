53abe87b9fd96af82f068f940f68e977ce9dc069
jemelk moeder <jboenink@outlook.com>
2025-06-23 12:06:50 +0200

    Update AIproject.py

--- AIproject.py
+++ AIproject.py
@@ -10,8 +10,8 @@ jokes = df["Joke"].dropna()
 # Streamlit interface
 st.title("Joke generator")
 
-censor = st.checkbox("Censureer ongepaste taal")
-subject = st.text_input("Over welk onderwerp wil je een grap horen?")
+censor = st.checkbox("Do you want to censor the jokes? (yes or no) ")
+subject = st.text_input("What subject do you want to hear a joke about? ")
 
 if subject:
     lower_subject = subject.lower()
@@ -19,21 +19,21 @@ if subject:
     if censor:
         profanity.load_censor_words()
         if profanity.contains_profanity(lower_subject):
-            st.warning("Dat onderwerp is ongepast 😬")
+            st.warning("this subject is innapropriate 😬")
         else:
             results = jokes[jokes.str.lower().str.contains(lower_subject)]
             if not results.empty:
                 selected_joke = random.choice(results.tolist())
                 censored_joke = profanity.censor(selected_joke)
-                st.success("Hier komt 'ie:")
+                st.success("Here it comes:")
                 st.write(censored_joke)
             else:
-                st.error("Sorry, geen grappen gevonden over dat onderwerp 😅")
+                st.error("Sorry, no jokes found about that subject 😅")
     else:
         results = jokes[jokes.str.lower().str.contains(lower_subject)]
         if not results.empty:
             selected_joke = random.choice(results.tolist())
-            st.success("Hier komt 'ie:")
+            st.success("Here it comes:")
             st.write(selected_joke)
         else:
-            st.error("Sorry, geen grappen gevonden over dat onderwerp 😅")
+            st.error("Sorry, no jokes found about that subject 😅")
